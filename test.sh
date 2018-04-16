a1f164617e5b0245a662114c8abf65af904f5e90
sijuntan <sijuntan@yeah.net>
2018-04-16 17:21:14 -0400

    add test.sh

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+MODE="test"
+CONTENT_FPATH="./rotunda.jpg"
+CKPT_FPATH='./checkpoint/epoch_2_Mon_Apr_16_11:03:07_2018.model'
+CKPT_FPATH2='./checkpoint/epoch_2_Mon_Apr_16_01:40:15_2018.model'
+
+python train.py $MODE --ckpt_fpath $CKPT_FPATH --content_fpath $CONTENT_FPATH
